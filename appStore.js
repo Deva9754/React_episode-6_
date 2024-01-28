cc50f575c287b00425208b749cae869af12b2172
Deva9754 <131431595+Deva9754@users.noreply.github.com>
2024-01-28 23:03:24 +0530

    Add files via upload

---
+++ appStore.js
@@ -0,0 +1,11 @@
+import { configureStore, createReducer } from "@reduxjs/toolkit";
+import cartReducer from "./cardSlice"
+
+
+ const appStore = configureStore({
+    reducer:{
+        cart: cartReducer,
+    },
+ });
+
+ export default appStore;
